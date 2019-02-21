89db028761a464b5be45d67c5148290d5fa0aaf3
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-21 13:10:53 +0100

    Solve properties about the framework

--- WeCodePropertiesSwiftTests/PlayingWithLibrarySpec.swift
+++ WeCodePropertiesSwiftTests/PlayingWithLibrarySpec.swift
@@ -15,30 +15,62 @@ import SwiftCheck
 class PlayingWithLibrarySpec: XCTestCase {
 
     func testAddingLogsToSeeHowItExecutes() {
-        // write test the whatever property about the sum but add logs to know the value of the received numbers
+        property("add logs to know how it executes")
+            <- (forAll { (_: Int, _: String) in
+                return true
+        }).verbose
     }
 
     func testModifyNumberOfExecutions() {
-        // write a test in order to ensure the the number of executions
-        // TIP: Check the paramters accepted by forAll to change the number of executions
+        var executed = 0
+        let expectedExectutions = 25
+        property("allow us to modify the number of executions",
+                 arguments: CheckerArguments(maxAllowableSuccessfulTests: expectedExectutions))
+            <- (forAll { (_: Int) in
+                executed += 1
+                return true
+            })
+        XCTAssertEqual(executed, expectedExectutions)
     }
 
     func testShrinkByMakingATestFail() {
-        //write a property that fails and check how the shrink works and what information is provided by the library
-        //I dind't find how to capture when a property fails to avoid breaking the build when we force a failure :(
+        property("make a test fail in order to see how shrink works")
+            <- (forAll { (anyNumber: Int) in
+                return anyNumber == 0
+                }).verbose.expectFailure
     }
 
-    func testRestrictiveGenerator() {
-        // Not sure whether this case can be generated in SwiftCheck as long as I didn't find a filter option.
-        // Seems like Gen doesn't return nulable generate data but ensure a reslt.
-        // Here you'll find how to implement a custom generator: https://github.com/typelift/SwiftCheck#custom-types.
+    func testProvidingSeedShouldRepeatTheExecution() {
+        //based on: https://github.com/typelift/SwiftCheck/blob/master/Tests/SwiftCheckTests/ReplaySpec.swift#L15
+        let genWithSeed = CheckerArguments(replay: .some((StdGen(2353135, 233), 1234)))
+
+        var usedArgsForProperty1: [Int] = []
+        property("execution 1 with seeed", arguments: genWithSeed)
+            <- (forAll { (anyNumber: Int) in
+                usedArgsForProperty1.append(anyNumber)
+                return true
+            })
+
+        var usedArgsForProperty2: [Int] = []
+        property("execution 2 with seeed", arguments: genWithSeed)
+            <- (forAll { (anyNumber: Int) in
+                usedArgsForProperty2.append(anyNumber)
+                return true
+            })
+
+        XCTAssertEqual(usedArgsForProperty1, usedArgsForProperty2)
     }
 
     func testCustomGenerator() {
-        // write a custom generator that only generate a very limited range of values.
-        // Tip: You can use an already defined generator or just implement yours. I'll do mine :).
-        // Pay attention to map method to implement a non-restrictive generator.
-        // Here you'll find how to implement a custom generator: https://github.com/typelift/SwiftCheck#custom-types.
+        func range(min: Int, max: Int) -> Gen<Int> {
+            return Int.arbitrary.map { generated in
+                return (abs(generated) % (max - min)) + min
+            }
+        }
+        property("pass when a generator is limited but not restrictive")
+            <- (forAll(range(min: 10, max: 15)) { (anyNumber: Int) in
+                return anyNumber >= 10 && anyNumber < 15
+            })
     }
 
 }
