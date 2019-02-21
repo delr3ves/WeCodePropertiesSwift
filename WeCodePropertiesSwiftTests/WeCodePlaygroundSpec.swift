6cc67512d1da4fc00d7cfc6503f5b948b95d4ada
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-21 10:29:08 +0100

    Avoid adding labels to each sum call

--- WeCodePropertiesSwiftTests/WeCodePlaygroundSpec.swift
+++ WeCodePropertiesSwiftTests/WeCodePlaygroundSpec.swift
@@ -13,34 +13,34 @@ import SwiftCheck
 @testable import WeCodePropertiesSwift
 
 class WeCodePlaygroundSpec: XCTestCase {
-    func sum(op1: Int, op2: Int) -> Int {
+    func sum(_  op1: Int, _ op2: Int) -> Int {
         return op1 + op2
     }
 
     func testAssociativeProperty() {
         property("should keep the associative property")
             <- forAll { (num1: Int, num2: Int, num3: Int) in
-                return self.sum(op1: self.sum(op1: num1, op2: num2), op2: num3)
-                    == self.sum(op1: num1, op2: self.sum(op1: num2, op2: num3))
+                return self.sum(self.sum(num1, num2), num3)
+                    == self.sum(num1, self.sum(  num2, num3))
         }
 
         property("Sum keep the commutative property")
             <- forAll { (num1: Int, num2: Int) in
-                return self.sum(op1: num1, op2: num2) == self.sum(op1: num1, op2: num2)
+                return self.sum(  num1, num2) == self.sum(  num1, num2)
         }
 
         property("Sum have an identity value")
             <- forAll { (num1: Int) in
-                return self.sum(op1: num1, op2: 0) == num1
+                return self.sum(  num1, 0) == num1
         }
     }
 
     func testSumWithSpecificValues() {
-        XCTAssertEqual(sum(op1: 1, op2: 4), 5)
-        XCTAssertEqual(sum(op1: 4, op2: 5), 9)
-        XCTAssertEqual(sum(op1: 0, op2: 0), 0)
-        XCTAssertEqual(sum(op1: -1, op2: -5), -6)
-        XCTAssertEqual(sum(op1: 1, op2: -5), -4)
-        XCTAssertEqual(sum(op1: -5, op2: 1), -4)
+        XCTAssertEqual(sum(  1, 4), 5)
+        XCTAssertEqual(sum(  4, 5), 9)
+        XCTAssertEqual(sum(  0, 0), 0)
+        XCTAssertEqual(sum(  -1, -5), -6)
+        XCTAssertEqual(sum(  1, -5), -4)
+        XCTAssertEqual(sum(  -5, 1), -4)
     }
 }
