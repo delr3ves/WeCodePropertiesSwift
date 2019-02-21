c24902425aa092e8c186017a5d6500dc0cbe8e8d
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-02-21 13:14:43 +0100

    Define properties to play with the framework

--- WeCodePropertiesSwiftTests/PlayingWithLibrarySpec.swift
+++ WeCodePropertiesSwiftTests/PlayingWithLibrarySpec.swift
@@ -20,24 +20,23 @@ class PlayingWithLibrarySpec: XCTestCase {
 
     func testModifyNumberOfExecutions() {
         // write a test in order to ensure the the number of executions
-        // TIP: Check the paramters accepted by forAll to change the number of executions
+        // TIP: Check the paramters accepted by property to change the number of executions
     }
 
     func testShrinkByMakingATestFail() {
         //write a property that fails and check how the shrink works and what information is provided by the library
-        //I dind't find how to capture when a property fails to avoid breaking the build when we force a failure :(
+        //TIP: forAll method return a Prop that allows us to capture the error
     }
 
-    func testRestrictiveGenerator() {
-        // Not sure whether this case can be generated in SwiftCheck as long as I didn't find a filter option.
-        // Seems like Gen doesn't return nulable generate data but ensure a reslt.
-        // Here you'll find how to implement a custom generator: https://github.com/typelift/SwiftCheck#custom-types.
+    func testProvidingSeedShouldRepeatTheExecution() {
+        //property method allow use to provide arguments: CheckerArguments. Let's take a look to replay field :)
+        //based on: https://github.com/typelift/SwiftCheck/blob/master/Tests/SwiftCheckTests/ReplaySpec.swift#L15
     }
 
     func testCustomGenerator() {
         // write a custom generator that only generate a very limited range of values.
         // Tip: You can use an already defined generator or just implement yours. I'll do mine :).
-        // Pay attention to map method to implement a non-restrictive generator.
+        // Pay attention to MAP method to implement a non-restrictive generator.
         // Here you'll find how to implement a custom generator: https://github.com/typelift/SwiftCheck#custom-types.
     }
 
